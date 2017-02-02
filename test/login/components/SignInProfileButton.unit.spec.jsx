710ff28f1b0b05cef23b2ab604885ebcd2cc9409
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2017-02-02 10:12:40 -0500

    adds more tests to login link

--- test/login/components/SignInProfileButton.unit.spec.jsx
+++ test/login/components/SignInProfileButton.unit.spec.jsx
@@ -17,16 +17,42 @@ describe('<SignInProfileButton>', () => {
     expect(vdom).to.not.be.undefined;
   });
 
-  it('should present login link when currentlyLoggedIn is false', () => {
+  it('should display signin or signup links when user is not logged in.', () => {
     const link = tree.everySubTree('a');
     expect(link).to.have.lengthOf(2);
   });
 
-  it('should present profile and sign out buttons when currentlyLoggedIn is true', () => {
-    const signedInData = { currentlyLoggedIn: true, loginUrl: '' };
-    const profileData = { email: 'fake@aol.com', userFullName: { first: 'Sharon' } };
-    tree = SkinDeep.shallowRender(<SignInProfileButton login={signedInData} profile={profileData}/>);
+  it('should display email address when user is logged in but not has not identity proofed.', () => {
+    // const link = tree.everySubTree('a');
+    // expect(link).to.have.lengthOf(2);
+  });
+
+  it('should display first name when user is logged in and has identity proofed.', () => {
+    // const link = tree.everySubTree('a');
+    // expect(link).to.have.lengthOf(2);
+  });
+
+  const signedInData = { currentlyLoggedIn: true, loginUrl: '' };
+  const verifiedProfileData = { userFullName: { first: 'Sharon' } };
+  const nonVerifiedProfileData = { email: 'fake@aol.com', userFullName: { first: null } };
+
+
+  it('should display profile, and signout links when user is logged in.', () => {
+    tree = SkinDeep.shallowRender(<SignInProfileButton login={signedInData} profile={nonVerifiedProfileData}/>);
     const link = tree.everySubTree('a');
     expect(link).to.have.lengthOf(2);
+  });
+
+  it('should display email when user is logged in and first name has not been provided.', () => {
+    tree = SkinDeep.shallowRender(<SignInProfileButton login={signedInData} profile={nonVerifiedProfileData}/>);
+    const greeting = tree.everySubTree('.greeting');
+    expect(greeting).to.have.lengthOf(1);
+    expect(greeting).to.equal('fake@aol.com');
+  });
+
+  it('should display first name, profile, and signout links when user is logged in and first name has been provided.', () => {
+    tree = SkinDeep.shallowRender(<SignInProfileButton login={signedInData} profile={verifiedProfileData}/>);
+    const greeting = tree.everySubTree('.greeting');
+    expect(greeting).to.have.lengthOf(1);
   });
 });
