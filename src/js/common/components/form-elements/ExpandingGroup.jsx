9664a3483179d80f7b3fbf4b3d5e01f5a6a98c63
Jeff Balboni <jeff@adhocteam.us>
2017-04-19 23:05:14 -0400

    Transition group

--- src/js/common/components/form-elements/ExpandingGroup.jsx
+++ src/js/common/components/form-elements/ExpandingGroup.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import classnames from 'classnames';
 
 /*
